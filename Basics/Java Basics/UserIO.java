755a05cc2fba8d75af84b99498619e8c20844ac9
Manish Anand <89120380+firem90@users.noreply.github.com>
2023-08-01 10:35:20 +0530

    Create UserIO.java
    https://www.codingninjas.com/studio/problems/find-character-case_58513?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf

---
+++ Basics/Java Basics/UserIO.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner sc= new Scanner(System.in);
+        String s = sc.next();
+        char c = s.charAt(0);
+        if(c - 65 >= 0 && c - 65 <= 26) {
+            System.out.println(1);
+        } else if(c-97 >= 0 && c-97 <= 26) {
+            System.out.println(0);
+        } else 
+            System.out.println(-1);
+
+    }
+}
