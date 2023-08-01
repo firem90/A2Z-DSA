40c355bd4cf3e1b7989c8fe0dd6f83af8826a680
Manish Anand <89120380+firem90@users.noreply.github.com>
2023-08-01 10:42:21 +0530

    Add files via upload

---
+++ Basics/Patterns/Pattern_01.java
@@ -0,0 +1,31 @@
+package Patterns;
+
+import java.util.Scanner;
+
+/*
+ Pattern :
+    *****
+    *****
+    *****
+    *****
+    *****
+*/
+public class Pattern_01 {
+    public static void pattern(int n) {
+        for (int i = 0; i != n; i++) { // rows
+            for (int j = 0; j != n; j++) { // columns
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int number = scanner.nextInt();
+        pattern(number);
+        scanner.close();
+    }
+
+}
